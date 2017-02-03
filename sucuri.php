d653a33f4d7111e034ec3a5d42c2bccd6ec4609d
Cixtor <cixtords@gmail.com>
2017-02-03 09:00:00 -0600

    Fix loading sequence for additional PHP files
    After we split the giant PHP file from previous versions of the plugin
    we experienced some issues with the loading of the new files from the
    new subdirectory. This commit fixes these issues and leaves the plugin
    working as it was before but with the new project structure.

--- sucuri.php
+++ sucuri.php
@@ -4,7 +4,7 @@ Plugin Name: Sucuri Security - Auditing, Malware Scanner and Hardening
 Plugin URI: https://wordpress.sucuri.net/
 Description: The <a href="https://sucuri.net/" target="_blank">Sucuri</a> plugin provides the website owner the best Activity Auditing, SiteCheck Remote Malware Scanning, Effective Security Hardening and Post-Hack features. SiteCheck will check for malware, spam, blacklisting and other security issues like .htaccess redirects, hidden eval code, etc. The best thing about it is it's completely free.
 Author: Sucuri, INC
-Version: 1.8.2
+Version: 1.8.3
 Author URI: https://sucuri.net
 */
 
@@ -53,8 +53,123 @@ foreach ($sucuriscan_dependencies as $dependency) {
     }
 }
 
-require_once('src/constants.php');
-require_once('src/globals.php');
+/**
+ * Plugin's constants.
+ *
+ * These constants will hold the basic information of the plugin, file/folder
+ * paths, version numbers, read-only variables that will affect the functioning
+ * of the rest of the code. The conditional will act as a container helping in
+ * the readability of the code considering the total number of lines that this
+ * file will have.
+ */
+
+/**
+ * Unique name of the plugin through out all the code.
+ */
+define('SUCURISCAN', 'sucuriscan');
+
+/**
+ * Current version of the plugin's code.
+ */
+define('SUCURISCAN_VERSION', '1.8.3');
+
+/**
+ * The name of the Sucuri plugin main file.
+ */
+define('SUCURISCAN_PLUGIN_FILE', 'sucuri.php');
+
+/**
+ * The name of the folder where the plugin's files will be located.
+ *
+ * Note that we are using the constant FILE instead of DIR because some
+ * installations of PHP are either outdated or are not supporting the access to
+ * that definition, to keep things simple we will select the name of the
+ * directory name of the current file, then select the base name of that
+ * directory.
+ */
+define('SUCURISCAN_PLUGIN_FOLDER', basename(dirname(__FILE__)));
+
+/**
+ * The fullpath where the plugin's files will be located.
+ */
+define('SUCURISCAN_PLUGIN_PATH', WP_PLUGIN_DIR.'/'.SUCURISCAN_PLUGIN_FOLDER);
+
+/**
+ * The fullpath of the main plugin file.
+ */
+define('SUCURISCAN_PLUGIN_FILEPATH', SUCURISCAN_PLUGIN_PATH.'/'.SUCURISCAN_PLUGIN_FILE);
+
+/**
+ * The local URL where the plugin's files and assets are served.
+ */
+define('SUCURISCAN_URL', rtrim(plugin_dir_url(SUCURISCAN_PLUGIN_FILEPATH), '/'));
+
+/**
+ * Checksum of this file to check the integrity of the plugin.
+ */
+define('SUCURISCAN_PLUGIN_CHECKSUM', @md5_file(SUCURISCAN_PLUGIN_FILEPATH));
+
+/**
+ * Remote URL where the public Sucuri API service is running.
+ */
+define('SUCURISCAN_API', 'sucuri://wordpress.sucuri.net/api/');
+
+/**
+ * Latest version of the public Sucuri API.
+ */
+define('SUCURISCAN_API_VERSION', 'v1');
+
+/**
+ * Remote URL where the CloudProxy API service is running.
+ */
+define('SUCURISCAN_CLOUDPROXY_API', 'sucuri://waf.sucuri.net/api');
+
+/**
+ * Latest version of the CloudProxy API.
+ */
+define('SUCURISCAN_CLOUDPROXY_API_VERSION', 'v2');
+
+/**
+ * The maximum quantity of entries that will be displayed in the last login page.
+ */
+define('SUCURISCAN_LASTLOGINS_USERSLIMIT', 25);
+
+/**
+ * The maximum quantity of entries that will be displayed in the audit logs page.
+ */
+define('SUCURISCAN_AUDITLOGS_PER_PAGE', 50);
+
+/**
+ * The maximum quantity of buttons in the paginations.
+ */
+define('SUCURISCAN_MAX_PAGINATION_BUTTONS', 20);
+
+/**
+ * The minimum quantity of seconds to wait before each filesystem scan.
+ */
+define('SUCURISCAN_MINIMUM_RUNTIME', 10800);
+
+/**
+ * The life time of the cache for the results of the SiteCheck scans.
+ */
+define('SUCURISCAN_SITECHECK_LIFETIME', 1200);
+
+/**
+ * The life time of the cache for the results of the get_plugins function.
+ */
+define('SUCURISCAN_GET_PLUGINS_LIFETIME', 1800);
+
+/**
+ * The maximum execution time of a HTTP request before timeout.
+ */
+define('SUCURISCAN_MAX_REQUEST_TIMEOUT', 15);
+
+/**
+ * The maximum execution time for SiteCheck requests before timeout.
+ */
+define('SUCURISCAN_MAX_SITECHECK_TIMEOUT', 60);
+
+/* Load all classes before anything else. */
 require_once('src/sucuriscan.lib.php');
 require_once('src/request.lib.php');
 require_once('src/fileinfo.lib.php');
@@ -67,21 +182,31 @@ require_once('src/mail.lib.php');
 require_once('src/template.lib.php');
 require_once('src/fsscanner.lib.php');
 require_once('src/heartbeat.lib.php');
+require_once('src/hardening.lib.php');
 require_once('src/interface.lib.php');
+
+/* Load global variables and triggers */
+require_once('src/globals.php');
+
+/* Load handlers for main pages. */
 require_once('src/modfiles.php');
 require_once('src/sitecheck.php');
 require_once('src/firewall.php');
-require_once('src/hardening.lib.php');
 require_once('src/hardening.php');
 require_once('src/homepage.php');
 require_once('src/auditlogs.php');
 require_once('src/outdated.php');
 require_once('src/corefiles.php');
 require_once('src/posthack.php');
+
+/* Load handlers for main pages (lastlogins). */
 require_once('src/lastlogins.php');
 require_once('src/lastlogins-loggedin.php');
 require_once('src/lastlogins-failed.php');
 require_once('src/lastlogins-blocked.php');
+
+/* Load handlers for main pages (settings). */
+require_once('src/settings.php');
 require_once('src/settings-handler.php');
 require_once('src/settings-general.php');
 require_once('src/settings-scanner.php');
@@ -94,5 +219,6 @@ require_once('src/settings-apiservice.php');
 require_once('src/settings-selfhosting.php');
 require_once('src/settings-trustip.php');
 require_once('src/settings-heartbeat.php');
-require_once('src/settings.php');
+
+/* Load handlers for main pages (infosys). */
 require_once('src/infosys.php');
