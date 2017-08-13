b88fd357e853bb6d0f0b45d5f7e13559bffb2e52
Cixtor <cixtords@gmail.com>
2017-08-13 09:00:00 -0700

    Add language files to the list of ignored changes

--- src/integrity.lib.php
+++ src/integrity.lib.php
@@ -662,6 +662,8 @@ class SucuriScanIntegrity
             '^wp-content\/(themes|plugins)\/.+',
             '^google[0-9a-z]{16}\.html$',
             '^pinterest-[0-9a-z]{5}\.html$',
+            '^wp-content\/languages\/.+\.mo$',
+            '^wp-content\/languages\/.+\.po$',
             '\.ico$',
         );
 
