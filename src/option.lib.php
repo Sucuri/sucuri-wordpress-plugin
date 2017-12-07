26f3ff4e6a013d7a19be0172490d9eb8324bc618
Frankie Jarrett <fjarrett@gmail.com>
2017-12-07 11:11:59 -0600

    Code formatting to match project

--- src/option.lib.php
+++ src/option.lib.php
@@ -104,7 +104,7 @@ class SucuriScanOption extends SucuriScanRequest
             'sucuriscan_use_wpmail' => 'enabled',
         );
 
-        return (array) apply_filters( 'sucuriscan_option_defaults', $defaults );
+        return (array) apply_filters('sucuriscan_option_defaults', $defaults);
     }
 
     /**
